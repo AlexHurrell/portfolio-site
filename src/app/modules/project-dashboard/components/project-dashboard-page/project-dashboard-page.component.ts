55fd06aca79cf6dcdb3a2bc0f6e1a9f1a2956b0a
AlexHurrell <39062336+AlexHurrell@users.noreply.github.com>
2021-08-22 19:48:07 +0200

    updated project cards

--- src/app/modules/project-dashboard/components/project-dashboard-page/project-dashboard-page.component.ts
+++ src/app/modules/project-dashboard/components/project-dashboard-page/project-dashboard-page.component.ts
@@ -9,35 +9,35 @@ import { ProjectCard } from '../../models/models';
 export class ProjectDashboardPageComponent implements OnInit {
   projects: ProjectCard[] = [
     {
-      title: 'example title',
-      description: 'my example description',
-      picture: 'examplepic.jpg',
-      githubLink: 'https://google.com',
-      siteLink: 'https://google.com',
+      title: 'Simple Chat',
+      description:
+        "A quick mock chat app that sends and 'receives' messages in a window.",
+      picture: 'assets/simple_chat.JPG',
+      githubLink: 'https://github.com/AlexHurrell/WD-ALEX-HURRELL',
     },
     {
       title: 'example title 2',
       description: 'my example description 2',
-      picture: 'examplepic.jpg',
+      picture: 'assets/mock.jpg',
       githubLink: 'https://google.com',
       siteLink: 'https://google.com',
     },
     {
       title: 'example title 3',
       description: 'my example description 3',
-      picture: 'examplepic.jpg',
+      picture: 'assets/mock.jpg',
     },
     {
       title: 'example title 4',
       description: 'my example description 4',
-      picture: 'examplepic.jpg',
+      picture: 'assets/mock.jpg',
       githubLink: 'https://google.com',
       siteLink: 'https://google.com',
     },
     {
       title: 'example title 5',
       description: 'my example description 5',
-      picture: 'examplepic.jpg',
+      picture: 'assets/mock.jpg',
       githubLink: 'https://google.com',
       siteLink: 'https://google.com',
     },
